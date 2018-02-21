2bbf1c16aaccb7c0ff09b38252d18a7ec773416f
Vikas Haritwal <vkhsharma@gmail.com>
2018-02-21 12:37:32 +0530

    VIkas Haritwal

---
+++ shoppingbackend/src/main/java/net/vkh/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.vkh.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
